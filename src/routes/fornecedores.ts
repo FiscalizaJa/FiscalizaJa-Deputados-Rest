c8a355bd010cc2a5486b5d36546f8dbff0d3b5e5
Victoreisdavid <65618679+Victoreisdavid@users.noreply.github.com>
2023-12-11 14:19:42 -0300

    Feat: Recurso experimental de consulta de fornecedores

--- src/routes/fornecedores.ts
+++ src/routes/fornecedores.ts
@@ -4,6 +4,11 @@ import schemas from "../schemas/routes";
 
 const database = UseDatabase()
 
+/**
+ * SEARCHING ALGORITHM
+ * Due to invalid CNPJS, names mispelled and other various irregularities in expenses data, i needed to make a custom way to search, to ensure that the max possible number of expenses can be reached
+ */
+
 const FORNECEDOR_MAX = 150
 
 export default async function load(app: FastifyInstance) {
@@ -17,7 +22,8 @@ export default async function load(app: FastifyInstance) {
         const query: {
             idDeputado?: string,
             itens?: string,
-            pagina?: string
+            pagina?: string,
+            mes?: number | number[]
         } = req.query
 
         params.cnpj = params.cnpj.replace(/[^0-9]/g, "")
@@ -29,31 +35,55 @@ export default async function load(app: FastifyInstance) {
             })
         }
 
+        if(Array.isArray(query.mes)) {
+            if(query.mes.length > 12) {
+                return res.status(400).send({
+                    error: "Você pode especificar, no máximo até 12 meses"
+                })
+            }
+
+            query.mes = query.mes.filter(m => Number(m) <= 12 && Number(m) >= 1)
+        } else {
+            if(query.mes) {
+                if(Number(query.mes) > 12 || Number(query.mes) < 1) {
+                    return res.status(400).send({
+                        error: ""
+                    })
+                }
+
+                query.mes = [query.mes]
+            } else {
+                query.mes = new Array(12).fill(0).map((_, index) => (index + 1));
+            }
+        }
+
         query.itens = Math.max(Number(query.itens) || 0, FORNECEDOR_MAX).toString()
 
         const offset = Number(query.itens) * Number(query.pagina)
 
+        const search_for = await database`
+            SELECT DISTINCT fornecedor
+            FROM "Despesas"
+            WHERE "cnpjCPF" = ${params.cnpj}
+        `
+
         const fornecedores = await database`
             SELECT
             ano,
-            SUM("valorLiquido") AS "valorTotal"
+            SUM("valorLiquido") AS "valorTotal",
+            ARRAY_AGG(DISTINCT "mes") AS meses
             FROM "Despesas"
-            WHERE "cnpjCPF" = ${params.cnpj}
+            WHERE "fornecedor" IN ${database(search_for.map(f => f.fornecedor))}
+            AND mes IN ${database(query.mes as any)}
             ${query.idDeputado ? database`AND "numeroDeputadoID" = ${query.idDeputado}` : database``}
             GROUP BY ano
             LIMIT ${query.itens}
             ${Number(query.pagina) > 1 ? database`OFFSET ${offset}` : database``}
         `
 
-        const nome = await database`
-            SELECT fornecedor
-            FROM "Despesas"
-            WHERE "cnpjCPF" = ${params.cnpj}
-            LIMIT 1
-        `
-
         return {
-            fornecedor: nome[0]?.fornecedor || null,
+            nomesVariacoes: search_for?.map(f => f.fornecedor) || null,
+            meses: query.mes,
             data: fornecedores
         }
     })
